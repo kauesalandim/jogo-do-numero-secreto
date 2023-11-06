b7f589561b76f21a01d7846022f2e511f4fc5a73
Kauê Salandim <129309322+kauesalandim@users.noreply.github.com>
2023-11-05 22:35:53 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+function exibirTextoNaTela (tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto
+
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate: 1.2})
+}
+
+function exibirMensagemInicial(){
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+}
+
+exibirMensagemInicial();
+
+function verificarChute(){
+    let chute = document.querySelector('input').value;
+    
+    if(chute == numeroSecreto){
+        exibirTextoNaTela('h1', 'Você Acertou!');
+        let palavraTentativa = tentativas > 1? 'tentativas' : 'tentativa';
+        let mensagemTentativa = `Parabéns, você acertou o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativa);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    }
+
+    else{
+        if(chute > numeroSecreto){
+            exibirTextoNaTela('p', 'O número secreto é menor!');
+        }
+
+        else{
+            exibirTextoNaTela('p', 'O número secreto é maior!');
+        }
+        tentativas++
+        limparCampo();
+    }
+}
+function gerarNumeroAleatorio(){
+    return parseInt(Math.random() * 10 + 1);
+}
+
+
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo(){
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
